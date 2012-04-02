95b6e8849ce91181526ad458d71ba8761991dc93
Joseph <joseph@thellama.us>
2012-04-01 23:03:31 -0400

    initial commit

---
+++ game.py
@@ -0,0 +1,207 @@
+import os, sys
+from random import randint, choice
+from math import sin, cos, radians
+
+import pygame
+from pygame import Rect, Color
+from pygame.sprite import Sprite
+
+from simpleanimation import SimpleAnimation
+from utils import Timer
+from vec2d import vec2d
+from widgets import Box, MessageBoard, Button
+
+class Game(object):
+    # Game parameters
+    BG_TILE_IMG = 'images/wood.png'
+    BUTTON_BGIMG = 'images/x.png'
+    SCREEN_WIDTH, SCREEN_HEIGHT = 580, 500
+    GRID_SIZE = 20
+    FIELD_SIZE = 400, 400
+    
+    #will implement resource loading here
+    
+    #CREEP_FILENAMES = [
+    #    ('images/bluecreep_0.png', 'images/bluecreep_45.png'),
+    #    ('images/greencreep_0.png', 'images/greencreep_45.png'),
+    #    ('images/yellowcreep_0.png', 'images/yellowcreep_45.png'),
+    #    ('images/pinkcreep_0.png', 'images/pinkcreep_45.png'),
+    #]
+    
+    #Arbitrary game constants. Will make cheating easy.
+    
+    MAX_N_CREEPS = 50
+
+    def __init__(self):
+        pygame.init()
+        
+        #set up screen and background
+        self.screen = pygame.display.set_mode(
+                        (self.SCREEN_WIDTH, self.SCREEN_HEIGHT), 0, 32)
+        self.tile_img = pygame.image.load(self.BG_TILE_IMG).convert_alpha()
+        self.tile_img_rect = self.tile_img.get_rect()
+	
+	#Drawing a handy MessageBoard widget
+	#Can use these for any text
+        self.tboard_text = ['This is a test.']
+        self.tboard_x = 20
+        self.tboard_y = 20
+        self.tboard_width = 125
+        self.tboard_height = 30
+        self.tboard_rect = Rect(self.tboard_x, self.tboard_y, self.tboard_width, self.tboard_height)
+        self.tboard_bgcolor = Color(50, 20, 0)
+        self.tboard = MessageBoard(self.screen,
+            rect=self.tboard_rect,
+            bgcolor=self.tboard_bgcolor,
+            border_width=4,
+            border_color=Color('black'),
+            text=self.tboard_text,
+	    padding=5,
+            font=('comic sans', 18),
+            font_color=Color('yellow'))
+    
+        self.button_bgimg = pygame.image.load(self.BUTTON_BGIMG).convert_alpha()
+        self.button_width = self.button_bgimg.get_width()
+        self.button_height = self.button_bgimg.get_height()
+        
+        #hopefully this will draw the button -15 pixels from the right end, +15 from the top 
+        #(hopefully giving us a nice X)
+        self.button_rect = Rect(self.tboard_width, self.tboard_y-15, self.button_width, self.button_height)
+        self.button = Button(self.screen,
+                                rect=self.button_rect,
+                                btntype='Close',
+                                bgimg=self.button_bgimg,
+                                attached=self.tboard)
+        
+        
+        self.clock = pygame.time.Clock()
+        self.paused = False
+
+	#spawning entities
+
+        self.options = dict(
+            draw_grid=False)
+    
+    def xy2coord(self, pos):
+        """ Convert a (x, y) pair to a (nrow, ncol) coordinate
+        """
+        x, y = (pos[0] - self.field_rect.left, pos[1] - self.field_rect.top)
+        return (int(y) / self.GRID_SIZE, int(x) / self.GRID_SIZE)
+    
+    def coord2xy_mid(self, coord):
+        """ Convert a (nrow, ncol) coordinate to a (x, y) pair,
+            where x,y is the middle of the square at the coord
+        """
+        nrow, ncol = coord
+        return (
+            self.field_rect.left + ncol * self.GRID_SIZE + self.GRID_SIZE / 2, 
+            self.field_rect.top + nrow * self.GRID_SIZE + self.GRID_SIZE / 2)
+    
+    def get_field_rect(self):
+        """ Return the internal field rect - the rect of the game
+            field exluding its border.
+        """
+        return self.field_box.get_internal_rect()
+    
+    def draw_background(self):
+        img_rect = self.tile_img.get_rect()
+        nrows = int(self.screen.get_height() / img_rect.height) + 1
+        ncols = int(self.screen.get_width() / img_rect.width) + 1
+        
+        for y in range(nrows):
+            for x in range(ncols):
+                img_rect.topleft = (x * img_rect.width, 
+                                    y * img_rect.height)
+                self.screen.blit(self.tile_img, img_rect)
+    
+    def draw_grid(self):
+        for y in range(self.grid_nrows + 1):
+            pygame.draw.line(
+                self.screen,
+                Color(50, 50, 50),
+                (self.field_rect.left, self.field_rect.top + y * self.GRID_SIZE - 1),
+                (self.field_rect.right - 1, self.field_rect.top + y * self.GRID_SIZE - 1))
+        
+        for x in range(self.grid_ncols + 1):
+            pygame.draw.line(
+                self.screen,
+                Color(50, 50, 50),
+                (self.field_rect.left + x * self.GRID_SIZE - 1, self.field_rect.top),
+                (self.field_rect.left + x * self.GRID_SIZE - 1, self.field_rect.bottom - 1))
+    
+    def draw(self):
+	#draw background image
+        self.draw_background()
+	
+	#draw game field crap here
+        
+	#decide if we should draw grid.
+        if self.options['draw_grid']:
+            self.draw_grid()
+                
+        #Only stuff being drawn right now.
+        #Message board with x button
+        self.tboard.draw()
+	self.button.draw()
+        
+	#this way we can draw dynamic MessageBoards.
+        #self.mboard.text = self.mboard_text <-- copy in latest text
+        #self.mboard.draw()
+        
+	#other entity draw calls
+        
+    def run(self):
+        # The main game loop
+        #
+        while True:
+            # Limit frame speed to 30 FPS
+            #
+            time_passed = self.clock.tick(30)
+            #~ time_passed = self.clock.tick()
+            #~ print time_passed
+            
+            # If too long has passed between two frames, don't
+            # update (the game must have been suspended for some
+            # reason, and we don't want it to "jump forward"
+            # suddenly)
+            #
+            if time_passed > 100:
+                continue
+            
+	    #Event loop. In-game control is pretty much routed through here
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.quit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.paused = not self.paused
+                    elif event.key == pygame.K_g:
+			pass
+			#toggle draw pathing grid
+                        #if pygame.key.get_mods() & pygame.KMOD_CTRL:
+                        #    self.options['draw_grid'] = not self.options['draw_grid']
+                elif (event.type == pygame.MOUSEBUTTONDOWN and event.button == 1):
+                        self.button.mouse_click_event(event.pos)
+			pass
+			#entity events here.
+
+            #update hud, counters, score, anything like that here
+            if not self.paused:
+                msg1 = ''
+                msg2 = ''
+                #update stats counters. Not doing anything yet
+                self.mboard_text = [msg1, msg2]
+
+		#update entities with time passed for internal calculations
+
+                self.draw()
+            #actually flip Surface buffers
+            pygame.display.flip()
+
+    def quit(self):
+        sys.exit()
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
---
+++ widgets.py
@@ -0,0 +1,215 @@
+""" Simplistic "GUI Widgets" for a Pygame screen.
+    
+    Most widgets receive a 'surface' argument in the constructor.
+    This is the pygame surface to which the widget will draw 
+    itself when it's draw() method is called.
+    
+    Unless otherwise specified, all rectangles are pygame.Rect
+    instances, and all colors are pygame.Color instances.
+"""
+import sys
+
+import pygame
+from pygame import Rect, Color
+
+
+class WidgetError(Exception): pass
+class LayoutError(WidgetError): pass
+
+
+class Box(object):
+    """ A rectangular box. Has a background color, and can have
+        a border of a different color.
+        
+        Has a concept of the "internal rect". This is the rect
+        inside the border (not including the border itself).
+    """
+    def __init__(self, 
+            surface,
+            rect,
+            bgcolor,
+            border_width=0,
+            border_color=Color('black')):
+        """ rect:
+                The (outer) rectangle defining the location and
+                size of the box on the surface.
+            bgcolor: 
+                The background color
+            border_width:
+                Width of the border. If 0, no border is drawn. 
+                If > 0, the border is drawn inside the bounding 
+                rect of the widget (so take this into account when
+                computing internal space of the box).
+            border_color:
+                Color of the border.
+        """
+        self.surface = surface
+        self.rect = rect
+        self.bgcolor = bgcolor
+        self.border_width = border_width
+        self.border_color = border_color
+        
+        # Internal rectangle
+        self.in_rect = Rect(
+            self.rect.left + self.border_width,
+            self.rect.top + self.border_width,
+            self.rect.width - self.border_width * 2,
+            self.rect.height - self.border_width * 2)
+        
+    def draw(self):
+        pygame.draw.rect(self.surface, self.border_color, self.rect)        
+        pygame.draw.rect(self.surface, self.bgcolor, self.in_rect)
+
+    def get_internal_rect(self):
+        """ The internal rect of the box.
+        """
+        return self.in_rect
+
+
+class MessageBoard(object):
+    """ A rectangular "board" for displaying messages on the 
+        screen. Uses a Box with text drawn inside.
+        
+        The text is a list of lines. It can be retrieved and 
+        changed with the .text attribute.
+    """
+    def __init__(self, 
+            surface,
+            rect,
+            text,
+	    padding,
+            font=('arial', 20),
+            font_color=Color('white'),
+            bgcolor=Color('gray25'),
+            border_width=0,
+            border_color=Color('black')):
+        """ rect, bgcolor, border_width, border_color have the 
+            same meaning as in the Box widget.
+            
+            text:
+                The initial text of the message board.
+            font:
+                The font (a name, size tuple) of the message
+            font_color:
+                The font color
+        """
+        self.surface = surface
+        self.rect = rect
+        self.text = text
+	self.padding = padding
+        self.bgcolor = bgcolor
+        self.font = pygame.font.SysFont(*font)
+        self.font_color = font_color
+        self.border_width = border_width
+        
+        self.box = Box(surface, rect, bgcolor, border_width, border_color)
+        
+    
+    def draw(self):
+        self.box.draw()
+        
+        # Internal drawing rectangle of the box 
+        #
+        text_rect = Rect(
+            self.rect.left + self.border_width,
+            self.rect.top + self.border_width,
+            self.rect.width - self.border_width * 2,
+            self.rect.height - self.border_width * 2)
+            
+        x_pos = text_rect.left
+        y_pos = text_rect.top 
+		
+        # Render all the lines of text one below the other
+        #
+        for line in self.text:
+            line_sf = self.font.render(line, True, self.font_color, self.bgcolor)
+            
+	    #test if we can fit text into the MessageBoard
+            if ( line_sf.get_width() + x_pos + self.padding > self.rect.right or line_sf.get_height() + y_pos + self.padding > self.rect.bottom):
+		raise LayoutError('Cannot fit line "%s" in widget' % line)
+		
+		#if ( self.padding > padding_boundary_right or self.padding > padding_boundary_bottom):
+		#	raise LayoutError('Padding will push text out of box, line was: "%s"' % line)
+            
+            self.surface.blit(line_sf, (x_pos+self.padding, y_pos+self.padding))
+            y_pos += line_sf.get_height()
+
+
+class Button(object):
+        """     Employs some crap from Box to draw a rectangular button, 
+                has some methods to handle click events.
+        """
+        
+        (UNCLICKED, CLICKING, CLICKED) = range(3)
+        
+        def __init__(self, surface, rect, btntype, bgimg, attached):
+                self.surface = surface
+                self.rect = rect
+                self.btntype = type
+                self.bgimg = bgimg
+                
+                self.width, self.height = self.bgimg.get_size()
+                
+                self.state = Button.UNCLICKED
+                
+                self.attached = attached
+                
+        def draw(self):
+                if self.is_visible():
+                        self.surface.blit(self.bgimg, self.rect)
+                
+        def mouse_click_event(self, pos):
+                if self.btntype == "Close":
+                        if self._point_is_inside(vec2d(pos)):
+                                self.state = Button.CLICKING
+                                Game.close_widget(self.attached)
+                                Button._close_widget(self)
+        
+        def is_visible(self):
+                return self.state == Button.UNCLICKED or self.state == Button.CLICKING
+        
+        #This will eventually be replaced with a more generic funciton
+        #so we can pass in a pos and 2d vector and tell if we've clicked on something
+        def _close_widget(self):
+                self.state == Button.CLICKED
+                self.kill()
+        
+        def _point_is_inside(self, point):
+                img_point = point - vec2d(  
+                        int(self.width / 2),
+                        int(self.width / 2))
+        
+                try:
+                        pix = self.image.get_at(img_point)
+                        return pix[3] > 0
+                except IndexError:
+                        return False
+                        
+##################################################################
+##################################################################
+
+if __name__ == "__main__":
+    pygame.init()
+    
+    SCREEN_WIDTH, SCREEN_HEIGHT = 350, 550
+    screen = pygame.display.set_mode(
+                (SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
+    clock = pygame.time.Clock()
+
+    box = Box(screen, Rect(20, 20, 40, 40), Color('brown4'), 0, Color('red'))
+    mb = MessageBoard(screen, Rect(80, 100, 200, 55), ["Bozo", "Jor"], 
+            border_width=2, border_color=Color('yellow'), font=('verdana', 16))
+
+    while True:
+        time_passed = clock.tick(30)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        
+        box.draw()
+        mb.draw()
+        
+        pygame.display.flip()
+    #~ pygame.font.SysFont
+
